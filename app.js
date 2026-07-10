873439e306eeb2e0956282493dbceb0407145314
Tarteel Hossam <tarteelhossam@gmail.com>
2026-07-10 22:30:55 +0300

    fixing 400 error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const AppError = require('./utils/AppError');
 const categoryRouter = require('./routes/categoryRoutes');
-const {connectDB} = require('./config/db');
+const { connectDB } = require('./config/db');
 const productRouter = require('./routes/productRoutes');
 const cartRouter = require('./routes/cartRoutes');
 const orderRouter = require('./routes/orderRoutes');
@@ -15,27 +15,21 @@ const app = express();
 app.use(express.json());
 
 connectDB();
+
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1/orders', orderRouter);
 
 app.use((req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-        stack: err.stack 
-    });
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
 module.exports = app;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,27 +1,20 @@
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
-  });
-};
+const AppError = require('./../utils/AppError');
 
 module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
   let error = { ...err };
   error.message = err.message;
-  error.stack = err.stack;
 
-  if (err.name === 'CastError') error = handleCastErrorDB(err);
-  if (err.code === 11000) error = handleDuplicateFieldsDB(err);
-  if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
-
-  error.statusCode = error.statusCode || err.statusCode || 500;
-  error.status = error.status || err.status || 'error';
-
-  if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(error, res);
-  } else {
-    sendErrorDev(error, res); 
+  if (err.code === 11000 || (err.name === 'MongoServerError' && err.message.includes('E11000'))) {
+    error.statusCode = 400;
+    error.message = 'Duplicate field value. Please use another value!';
   }
+
+  res.status(error.statusCode).json({
+    status: err.status,
+    message: error.message,
+    stack: err.stack
+  });
 };
